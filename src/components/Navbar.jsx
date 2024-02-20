772da4e5fce918ca00b5fa6b565b1dc05e64d48d
Emina Neimarlija <emina.neimarlija@smartsoftware.local>
2024-02-20 18:03:35 +0100

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar(){
+    return (
+        <nav>
+            <img  src="./src/assets/react-icon-small.png" className="nav-icon"/>
+            <h3 className="nav-logo-text">ReactFacts</h3>
+            <h4 className="nav-title">React Course - Project 1</h4>
+        </nav>
+    )
+}
